0cb11a74b721483f2d68f0bed1ac8ef6c6bd015a
Richard Yang <richard@rxyang.ca>
2020-06-15 22:13:09 -0600

    Add title to navbar

--- src/sections/navbar.js
+++ src/sections/navbar.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Link } from "gatsby";
 
 const Navbar = () => (
-  <nav>
+  <nav className="sticky top-0 flex justify-center bg-blue-500 py-4">
+    <div className="container text-center text-white font-bold text-2xl sm:text-3xl">
+      <Link to="/">
+        <span className="inline-block">Foothills Alliance Church&nbsp;</span>
+        <span className="inline-block">Intercultural Ministries</span>
+      </Link>
+    </div>
   </nav>
 );
 
