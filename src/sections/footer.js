74158788cff51175e19655b38f3cc9072322dce6
Richard Yang <richard@rxyang.ca>
2020-06-15 22:23:39 -0600

    Add footer

--- src/sections/footer.js
+++ src/sections/footer.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Footer = () => (
-  <footer>
+  <footer className="flex items-center justify-center bg-blue-400 py-6">
+    <div className="container">
+      <p className="text-center text-white">
+        <span className="inline-block">Copyright ⓒ&nbsp;</span>
+        <span className="inline-block">Foothills Alliance Church 2020</span></p>
+      <p className="text-center text-white">
+        Built with <span role="img" aria-label="love">❤️</span> by&nbsp;
+        <a href="https://rxyang.ca" target="_blank" rel="noreferrer" className="underline">Richard Yang</a>
+      </p>
+    </div>
   </footer>
 );
 
