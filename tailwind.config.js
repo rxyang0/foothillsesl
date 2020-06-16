5420464627ba3af16accbe3528706a51b0b694aa
Richard Yang <richard@rxyang.ca>
2020-06-15 22:18:36 -0600

    Center container with padding by default

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        default: '1rem',
+        sm: '2rem',
+      },
+    },
+  },
   purge: [
     './src/**/*.js',
   ],
