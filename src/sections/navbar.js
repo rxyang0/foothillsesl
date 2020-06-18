716a73822ab22bf32692af6acacb25197a4d04e7
Richard Yang <richard@rxyang.ca>
2020-06-18 13:25:08 -0600

    Smaller navbar height on mobile

--- src/sections/navbar.js
+++ src/sections/navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "gatsby";
 
 const Navbar = () => (
-  <nav className="sticky top-0 flex justify-center bg-blue-500 py-4">
+  <nav className="sticky top-0 flex justify-center bg-blue-500 py-2 sm:py-4">
     <div className="container text-center text-white font-bold text-2xl sm:text-3xl">
       <Link to="/">
         <span className="inline-block">Foothills Alliance Church&nbsp;</span>
