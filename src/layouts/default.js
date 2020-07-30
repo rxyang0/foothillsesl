a7ed935b709338459d9b790b0db7c2f233691f27
Richard Yang <richard@rxyang.ca>
2020-07-29 20:24:14 -0600

    Include favicon

--- src/layouts/default.js
+++ src/layouts/default.js
@@ -1,21 +1,22 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
 import "../css/style.css";
+import favicon from "../assets/favicon-192.png"
 import Navbar from "../sections/navbar";
 import Footer from "../sections/footer";
 
-const Default = ({ children, title, subtitle }) => {
-  return (
-    <>
-      <div className="flex flex-col min-h-screen">
-        <Navbar />
-        <main className="flex-1 bg-white">
-          {children}
-        </main>
-        <Footer />
-      </div>
-    </>
-  );
-}
-
-export default Default;
+export default ({ children }) => (
+  <>
+    <Helmet>
+      <link rel="icon" href={favicon}></link>
+    </Helmet>
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-1 bg-white">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  </>
+);
