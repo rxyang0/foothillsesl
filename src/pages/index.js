d97cb95dabc900164874bd47619c53ad23e48fca
Richard Yang <richard@rxyang.ca>
2020-08-13 10:52:32 -0600

    Update index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,27 @@
-import React from "react";
+import React from 'react';
 
-import Default from "../layouts/default";
-import SEO from "../sections/seo";
-import Header from "../sections/header";
-import ClassesSection from "../sections/classes";
-import EventsSection from "../sections/events";
+import styles from './index.module.scss';
+import Default from '../layouts/default';
+import SEO from '../components/seo';
+import Hero from '../sections/hero';
+import Table from '../components/table';
 
-const IndexPage = () => (
+export default () => (
   <Default>
     <SEO title="ESL Classes" />
-    <Header title="ESL Classes and More" subtitle="Welcome all beginner, intermediate, and advanced students!" />
-    <div className="container mx-auto py-16">
-      <p className="text-center text-lg sm:text-xl mx-auto text-gray-800 pb-16">
-        <span className="inline-block">
-          We are currently hosting the following classes via&nbsp;Zoom&nbsp;Online&nbsp;Meetings.
-        </span>
-        <span className="inline-block">
-          Please email{' '}
-          <a href="mailto:esl@foothillsalliance.com" className="underline text-blue-dark">esl@foothillsalliance.com</a>
-          {' '}to be added to the invitation&nbsp;emails.
-        </span>
-      </p>
-      <ClassesSection />
-      <EventsSection />
+    <Hero title="ESL Classes and More" subtitle="Welcome all beginner, intermediate, and advanced students!" />
+    <div className="container">
+      <section className={styles.message}>
+        <p>
+          We are currently hosting the following classes via Zoom online meetings.
+        </p>
+        <p>
+          Please email <a href="mailto:esl@foothillsalliance.com">esl@foothillsalliance.com</a>
+          {' '}to be added to the invitation emails.
+        </p>
+      </section>
+      <Table name="classes" />
+      <Table name="events" />
     </div>
   </Default>
 );
-
-export default IndexPage;
