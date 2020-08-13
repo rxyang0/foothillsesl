37dd8d39b5413092bd4e523f4efb9086b85f3618
Richard Yang <richard@rxyang.ca>
2020-08-13 10:42:11 -0600

    Write base variables and styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,16 @@
 module.exports = {
   siteMetadata: {
     title: `Foothills Intercultural Ministries`,
-    description: ``,
-    author: ``,
+    description: `ESL classes and more for beginner, intermediate, and advanced students!`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: 'gatsby-plugin-sass',
+      options: {
+        includePaths: ["src/styles/"],
+        data: `@import "variables";`,
+      }
+    },
   ],
 }
