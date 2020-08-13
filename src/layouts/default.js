156193f58696636f70f28a65fb3496022a5bbe18
Richard Yang <richard@rxyang.ca>
2020-08-13 10:44:36 -0600

    Update default layout

--- src/layouts/default.js
+++ src/layouts/default.js
@@ -1,22 +1,21 @@
-import React from "react";
-import { Helmet } from "react-helmet";
+import React from 'react';
+import { Helmet } from 'react-helmet';
 
-import "../css/style.css";
-import favicon from "../assets/favicon-192.png"
-import Navbar from "../sections/navbar";
-import Footer from "../sections/footer";
+import 'normalize.css';
+import '../styles/global.scss';
+import favicon from '../assets/favicon-192.png';
+import Navbar from '../sections/navbar';
+import Footer from '../sections/footer';
 
 export default ({ children }) => (
   <>
     <Helmet>
       <link rel="icon" href={favicon}></link>
     </Helmet>
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <main className="flex-1 bg-white">
-        {children}
-      </main>
-      <Footer />
-    </div>
+    <Navbar />
+    <main style={{backgroundColor: "#fff"}}>
+      {children}
+    </main>
+    <Footer />
   </>
 );
