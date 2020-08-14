f5cd8228ab7a667307404581eb5e7749fd3977de
Richard Yang <richard@rxyang.ca>
2020-08-13 22:18:57 -0600

    Use gatsby-image

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,9 +8,23 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-sass',
       options: {
-        includePaths: ["src/styles/"],
+        includePaths: [`./src/styles/`],
         data: `@import "variables";`,
       }
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        useMozJpeg: true,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `./content/assets/`,
+      },
     },
   ],
 }
