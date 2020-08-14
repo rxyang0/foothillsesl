5d67021f500c72cf5eda12d3f76b3daab025458d
Richard Yang <richard@rxyang.ca>
2020-08-13 22:19:24 -0600

    Add background image and styles to hero module

--- src/sections/hero.js
+++ src/sections/hero.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import Image from 'gatsby-image';
 
 import styles from './hero.module.scss';
 
-export default ({ title, subtitle }) => (
-  <header className={styles.header}>
-    <div className="container">
-      <h1 className={styles.title}>{title}</h1>
-      <h2 className={styles.subtitle}>{subtitle}</h2>
-    </div>
-  </header>
-);
+const query = graphql`
+  query {
+    heroLaptopMeeting: file(relativePath: { eq: "hero-laptop-meeting.jpg" }) {
+      childImageSharp {
+        fluid(quality: 80) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`;
+
+export default ({ title, subtitle }) => {
+  const data = useStaticQuery(query);
+  return (
+    <header className={styles.header}>
+      <Image fluid={data.heroLaptopMeeting.childImageSharp.fluid} alt="Laptop in online meeting"
+          className={styles.heroImage} />
+      <div className={`container ${styles.headerText}`}>
+        <h1 className={styles.title}>{title}</h1>
+        <h2 className={styles.subtitle}>{subtitle}</h2>
+      </div>
+    </header>
+  );
+}
