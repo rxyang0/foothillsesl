a553a260cf299be2d1cfdc4e71f8e5335c92681a
Richard Yang <richard@rxyang.ca>
2020-08-15 01:42:49 -0600

    Use redesigned classes and events section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,49 +1,24 @@
 import React from 'react';
-import { graphql } from 'gatsby';
 
 import styles from './index.module.scss';
 import Default from '../layouts/default';
 import SEO from '../components/seo';
 import Hero from '../sections/hero';
-import Table from '../components/table';
+import ClassesEvents from '../sections/classes-events';
 
-export default ({ data }) => (
+export default () => (
   <Default>
     <SEO title="ESL Classes" />
     <Hero title="ESL Classes and More" subtitle="Welcome all beginner, intermediate, and advanced students!" />
-    <div className="container">
-      <section className={styles.sectionMessage}>
-        <p>We are currently hosting the following classes via Zoom online meetings.</p>
-        <p>
-          Please email <a href="mailto:esl@foothillsalliance.com">esl@foothillsalliance.com</a>
-          {' '}to be added to the invitation emails.
-        </p>
-      </section>
-      <Table title="Classes" edges={data.allClassesYaml.edges} />
-      <Table title="Events" edges={data.allEventsYaml.edges} />
-    </div>
+    <section className={styles.sectionMessage}>
+      <div className="container">
+          <p>We are currently hosting the following classes via Zoom online meetings.</p>
+          <p>
+            Please email <a href="mailto:esl@foothillsalliance.com">esl@foothillsalliance.com</a>
+            {' '}to be added to the invitation emails.
+          </p>
+      </div>
+    </section>
+    <ClassesEvents />
   </Default>
 );
-
-export const query = graphql`
-  query {
-    allClassesYaml {
-      edges {
-        node {
-          title
-          date
-          description
-        }
-      }
-    }
-    allEventsYaml {
-      edges {
-        node {
-          title
-          date
-          description
-        }
-      }
-    }
-  }
-`;
