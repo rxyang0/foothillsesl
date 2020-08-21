63164064caec4259c88a749736401597b52bf12b
Richard Yang <richard@rxyang.ca>
2020-08-20 21:12:47 -0600

    Add animations to hero section

--- src/sections/hero.js
+++ src/sections/hero.js
@@ -1,7 +1,8 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useStaticQuery, graphql } from 'gatsby';
 import Image from 'gatsby-image';
+import { motion } from 'framer-motion';
 
 import styles from './hero.module.scss';
 
@@ -19,45 +20,44 @@ const query = graphql`
 
 export default function Hero({ title, subtitle }) {
   const data = useStaticQuery(query);
-  useEffect(() => {
-    const hero = document.querySelector(`.${styles.hero}`);
-    const heroBg = hero.querySelector(`.${styles.heroBackground}`);
-    const heroTransformInit = 'scale(1.03)';
-    const heroFilterInit = 'blur(2px)';
-    const heroTransformFinal = 'scale(1.1)';
-    const heroFilterFinal = 'blur(0)';
-    heroBg.style.transform = heroTransformInit;
-    heroBg.style.filter = heroFilterInit;
-    /* Chrome bug: changes to transform-origin have no effect until transform transition ends
-        https://stackoverflow.com/questions/57000539/chrome-75-break-transform-origin-animation */
-    // const handleMouseMove = (evt) => {
-    //   heroBg.style.transformOrigin = `${evt.clientX}px ${evt.clientY}px`;
-    // }
-    // hero.addEventListener('mousemove', handleMouseMove);
-    const handleMouseOver = () => {
-      heroBg.style.transform = heroTransformFinal;
-      heroBg.style.filter = heroFilterFinal;
-    };
-    const handleMouseOut = () => {
-      heroBg.style.transform = heroTransformInit;
-      heroBg.style.filter = heroFilterInit;
-    };
-    hero.addEventListener('mouseover', handleMouseOver);
-    hero.addEventListener('mouseout', handleMouseOut);
-    hero.addEventListener('touchstart', handleMouseOver, { passive: true });
-    hero.addEventListener('touchend', handleMouseOut, { passive: true });
-  });
+  const [hovering, setHovering] = useState(false);
   return (
     <header className={styles.hero}>
-      <Image
-        fluid={data.heroLaptopMeeting.childImageSharp.fluid}
-        alt="Laptop in online meeting"
+      <motion.div
         className={styles.heroBackground}
+        animate={{ scale: 1.03, filter: 'blur(2px)' }}
+        onMouseEnter={() => setHovering(true)}
+        onMouseLeave={() => setHovering(false)}
+        whileHover={{ scale: 1.15, filter: 'blur(0px)' }}
+        transition={{
+          scale: { ease: 'easeInOut', duration: 1.5 },
+          filter: { ease: 'easeInOut', duration: 0.5 },
+        }}
+      >
+        <Image
+          fluid={data.heroLaptopMeeting.childImageSharp.fluid}
+          alt="Laptop in online meeting"
+          className={styles.heroBackground}
+          draggable={false}
+        />
+      </motion.div>
+      <motion.div
+        className={styles.scrim}
+        animate={{ opacity: hovering ? 0 : 1 }}
+        transition={{ ease: 'easeInOut', duration: 0.5 }}
       />
-      <div className={styles.heroContent}>
+      <motion.div
+        className={styles.heroContent}
+        initial={{ scale: 0.9 }}
+        animate={{ scale: 1, opacity: hovering ? 0.6 : 1 }}
+        transition={{
+          scale: { ease: 'easeInOut', duration: 1.5 },
+          opacity: { ease: 'easeInOut', duration: 0.5 },
+        }}
+      >
         <h1 className={styles.title}>{title}</h1>
         <h2 className={styles.subtitle}>{subtitle}</h2>
-      </div>
+      </motion.div>
     </header>
   );
 }
